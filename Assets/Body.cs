7550524371a30937f9d5ef443eef60defd337df9
whitegrimreaper <theDumb12@yahoo.com>
2019-01-27 13:41:40 -0800

    Body simplified, organs and tissue systems not currently used

--- Assets/Body.cs
+++ Assets/Body.cs
@@ -6,14 +6,16 @@ public class Body {
 
     public class Limb
     {
-        public bool bone;
-        public double boneDamage;
-        public bool muscle;
-        public double muscleDamage;
-        public bool nerve;
-        public double nerveDamage;
-        public bool skin;
-        public double skinDamage;
+        //public bool bone;
+        //public double boneDamage;
+        //public bool muscle;
+        //public double muscleDamage;
+        //public bool nerve;
+        //public double nerveDamage;
+        //public bool skin;
+        //public double skinDamage;
+
+        public double damage;
 
         public bool bleeding;
         public double bleedingAmt;
@@ -28,17 +30,20 @@ public class Body {
             //Base init for human limb
             //just change the affect vars for different
             //types of limbs for now
-            bone = true;
-            boneDamage = 0.0f;
-            muscle = true;
-            muscleDamage = 0.0f;
-            nerve = true;
-            nerveDamage = 0.0f;
-            skin = true;
-            skinDamage = 0.0f;
+            //bone = true;
+            //boneDamage = 0.0f;
+            //muscle = true;
+            //muscleDamage = 0.0f;
+            //nerve = true;
+            //nerveDamage = 0.0f;
+            //skin = true;
+            //skinDamage = 0.0f;
+
+            damage = 0.0f;
 
             bleeding = false;
             bleedingAmt = 0.0f;
+
 
             affectWalk = false;
             affectManip = false;
@@ -47,13 +52,16 @@ public class Body {
         }
     }
 
-    public class Organ
-    {
-        //gonna flesh out later maybe
-    }
+    //public class Organ
+    //{
+    //    //gonna flesh out later maybe
+    //}
+
+    public double totalBlood = 1000f;
+    private double currBlood;
 
     public Limb[] limbs;
-    public Organ[] organs;
+    //public Organ[] organs;
 
     public Body()
     {
@@ -86,6 +94,8 @@ public class Body {
 
         limbs[5] = new Limb();//r leg
         limbs[5].affectWalk = true;
+
+        currBlood = totalBlood;
     }
 
     public Body(string race)
@@ -96,5 +106,28 @@ public class Body {
                 break;
         }
     }
-	
+
+    public void damagePart(int part, double damage, int type)
+    {
+        limbs[part].bleeding = true;
+        limbs[part].bleedingAmt += damage; // will depend on type
+
+        limbs[part].damage = +damage; //will need to factor in armor and shit later
+    }
+
+    public bool checkDead()
+    {
+        if(currBlood <= 0)
+        {
+            return true;
+        }
+        else if(limbs[0].damage >= 10.0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
