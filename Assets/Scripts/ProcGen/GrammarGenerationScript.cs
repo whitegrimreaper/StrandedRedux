9ef11851da40fc976df443b833c821a3917d24a4
theDumb12 <theDumb12@yahoo.com>
2019-05-15 23:53:17 -0700

    Some basic stuff from my desktop, no major changes

---
+++ Assets/Scripts/ProcGen/GrammarGenerationScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrammarGenerationScript : MonoBehaviour
+{
+
+    private class BaseGrammar
+    {
+        Rule[] rules;
+
+        public BaseGrammar()
+        {
+            //rules[0] = new Rule()
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ProcGen/Rule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rule
+{
+    string ruleName;
+    Rule[] expansion;
+    bool terminal;
+    public Room room;
+
+    public Rule(string ruleName, Rule[] expansion, bool terminal, GameObject room)
+    {
+        this.ruleName = ruleName;
+        this.terminal = terminal;
+    }
+
+    public Rule findRuleWithName(string findName)
+    {
+        return null;
+    }
+}
